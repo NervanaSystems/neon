c94ebf61e661b7e925a5cd9a4d68449c9c0c5f06
Alex Park <alex.park@gmail.com>
2015-05-12 00:17:25 -0700

    Allow reuse of example yamls for integration testing - Add integration command line option to neon executable - Adjust yaml options in integration mode - Among other things, drop serialization path, remove sources of randomization - Add is_random attribute to layers to identify drop-able layers   (Just dropout for now) - Make dotransforms have the correct behavior in Imageset - Change batch norm warnings to static to avoid proliferation of messages across many layers - Switch branch and small layers for cifar mlp since they were incorrectly labeled - Create integration test script in examples directory that uses existing metrics code - Correct i1k-alexnet-fp16 to use fp16 as backend data type

---
+++ examples/run_integration_tests.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------
+# Copyright 2014 Nervana Systems Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
+# Run all subset of examples sequentially, while collecting timing and
+# performance information.  Stats against prior runs are compared as well as
+# appended to the named file.
+THIS_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+OUT_FILE="${HOME}/.nervana/integration_test_results.tsv"
+LOG_FILE="${HOME}/.nervana/integration_test_results.log"
+NEON_EXE="${THIS_DIR}/../bin/neon"
+NEON_OPTS="-r 0 --integration -o ${OUT_FILE}"  # non-distributed
+CMP_EXE="${THIS_DIR}/../bin/compare_metrics"
+CMP_OPTS=""
+
+mkdir -p "$(dirname $OUT_FILE)"
+mkdir -p "$(dirname $LOG_FILE)"
+
+cpu_yaml=("recurrent/mobydick-lstm-small.yaml" \
+          "recurrent/mobydick-rnn-small.yaml")
+hpu_yaml=("convnet/i1k-alexnet-fp16.yaml")
+gpu_yaml=("convnet/i1k-alexnet-fp32.yaml")
+all_yaml=("convnet/mnist-small.yaml" "mlp/mnist-small.yaml" \
+          "convnet/cifar10-small.yaml" "mlp/cifar10-small.yaml")
+
+cpu_back=("cpu")
+hpu_back=("nervanagpu")
+gpu_back=("cudanet" "nervanagpu")
+all_back=("cpu" "cudanet" "nervanagpu")
+
+
+run_yamls()
+{
+  declare -a yamls=("${!1}")
+  declare -a backends=("${!2}")
+  for yaml in ${yamls[@]}
+  do
+    for backend in ${backends[@]}
+    do
+      echo "$(date) - Running: $yaml $backend..." | tee -a "$LOG_FILE"
+      PYTHONPATH="${THIS_DIR}/..:${PYTHONPATH}" $NEON_EXE $NEON_OPTS \
+      --gpu $backend "$yaml" >> "$LOG_FILE" 2>&1
+      if [ $? -eq 0 ]
+      then
+        PYTHONPATH="${THIS_DIR}/..:${PYTHONPATH}" \
+          $CMP_EXE $CMP_OPTS "$OUT_FILE" "$yaml"
+      else
+        echo "problems running $yaml"
+      fi
+      echo "" >> "$LOG_FILE"
+    done
+  done
+}
+
+run_yamls hpu_yaml[@] hpu_back[@]
+run_yamls gpu_yaml[@] gpu_back[@]
+run_yamls cpu_yaml[@] cpu_back[@]
+run_yamls all_yaml[@] all_back[@]
