ca33357c13f67ecd8313c8012cc9803217721546
Evren Tumer <evren@nervanasys.com>
2015-06-22 19:25:21 -0700

    added cpu->gpu handoff and gpu->cpu handoff tests

--- neon/tests/test_serialize.py
+++ neon/tests/test_serialize.py
@@ -53,65 +53,7 @@ class TestSerialization:
             self.model_path = os.path.join( os.getenv("HOME"), 'data') #~/data/
         return
 
-
-    def run_experiment_in_steps(self, config_file, N, k, be, **be_args): 
-        # run an experiment for N epochs in k stepe with
-        # N/k epochs per step.  Last step will be enough epochs
-        # to reach N total epochs.  Between each step the 
-        # model will be serialized and saved, then reloaded
-        # from that saved file at the next step
-
-        stepsize = N/k
-
-        for ind in range(1,k+1):
-            # run the same learning with N/k epochs k times
-            # each time saving and reloading the serialized model state
-            if ind == k:
-                end_epoch = N # in case N/k is a fraction, 
-                              # last step will end at epich N
-            else:
-                end_epoch = ind*stepsize
-
-            # load up base experiment config
-            experiment = deserialize( config_file )
-
-            # run for N/k steps
-            experiment.model.num_epochs = end_epoch
-
-            if ind > 1:
-                # after step 1 need to load initial config from last runs serialized file
-                experiment.model.deserialized_path = last_saved_state
-
-            # save the model to this file
-            last_saved_state = os.path.join(self.model_path,\
-                                        '%d_shot_%d.prm' %(k,end_epoch))
-            print last_saved_state
-            if os.path.exists(last_saved_state):
-                print 'removing %s' %last_saved_state
-                os.remove(last_saved_state)
-            experiment.model.serialized_path = last_saved_state
-
-            
-            experiment.model.serialize_schedule = k # may not be needed
-            backend = gen_backend(model=experiment.model, **be_args)
-            experiment.initialize(backend)
-
-            if ind == 1:
-                # save the initial weights for check with other runs
-                intial_weights={}
-                for ind,layer in enumerate(experiment.model.layers):
-                    if hasattr(layer,'weights'): # only looking at weights right now
-                        # make sure the names of each layer are unique by adding index
-                        intial_weights[ '%s_%d' %(layer.name, ind)  ] = \
-                                np.copy( layer.weights.asnumpyarray())
-
-            # run up to epoch `end_epoch`
-            res_2shot_half = experiment.run()
-
-        return ( last_saved_state  , intial_weights)
-
-
-
+    #---------------------------------------------------
     def test_toyModel_compare_cpuOnly(self):
         # init the toy model with known seed
         # save init weights locally for check later
@@ -150,18 +92,21 @@ class TestSerialization:
                 'final states of 1 vs %d step learning runs not matching' %k
         return
 
+    #---------------------------------------------------
+    @attr('cuda')  # can we do this test by test
     def test_toyModel_compare_gpuOnly(self):
-        N = 10
-        k = 2
+        # same as test_toyModel_compare_cpuOnly except all is run on the gpu
+        N = 50
+        k = 5
         atol=0.0
-        rtol=.1
+        rtol=.01
 
         config_file = os.path.join( self.script_dir, \
                 'toy_serialize_check_base.yml' )
 
         # run N steps in 1 shot 
         be='gpu'
-        be_args = {'rng_seed': self.seed, be : 'cudanet' }
+        be_args = {'rng_seed': self.seed, be : 'nervanagpu' }
         (fstate_1shot, init_w_1shot ) = self.run_experiment_in_steps(config_file, N, 1, be, **be_args)
 
         # run N steps in k shots
@@ -176,6 +121,177 @@ class TestSerialization:
         assert self.model_compare( fstate_1shot, fstate_kshot ,rtol=rtol, atol=atol),\
                 'final states of 1 vs %d step learning runs not matching' %k
         return
+
+    #---------------------------------------------------
+    @attr('cuda')  # can we do this test by test
+    def test_toyModel_compare_gpuOnly(self):
+        # same as test_toyModel_compare_cpuOnly except all is run on the gpu
+        N = 50
+        k = 5
+        atol=0.0
+        rtol=.01
+
+        config_file = os.path.join( self.script_dir, \
+                'toy_serialize_check_base.yml' )
+
+        # run N steps in 1 shot 
+        be='gpu'
+        be_args = {'rng_seed': self.seed, be : 'nervanagpu' }
+        (fstate_1shot, init_w_1shot ) = self.run_experiment_in_steps(config_file, N, 1, be, **be_args)
+
+        # run N steps in k shots
+        (fstate_kshot, init_w_kshot ) = self.run_experiment_in_steps(config_file, N, k, be, **be_args)
+
+
+        # comapre the initial weights
+        assert self.check_init_weights( init_w_kshot, init_w_1shot , rtol=rtol, atol=atol ),\
+                'initial model weights were not the same'
+
+        #compare the final model state for 1 step of N epochs vs. k steps of N/k epochs
+        assert self.model_compare( fstate_1shot, fstate_kshot ,rtol=rtol, atol=atol),\
+                'final states of 1 vs %d step learning runs not matching' %k
+        return
+
+
+    #---------------------------------------------------
+    @attr('cuda')  # can we do this test by test
+    def test_toyModel_compare_cpu_to_gpu(self):
+        # run whole test on cpu and run it 1/2 on cpu the 1/2 on gpu
+        # compare the outputs
+        N = 10 # for this tests make sure that N/k is not an integer
+        k = 2  # k should be 2 here
+        atol=0.0
+        rtol=.01
+
+        config_file = os.path.join( self.script_dir, \
+                'toy_serialize_check_base.yml' )
+
+        # run N steps in 2 shots and save the checkpoint which should be at int(N/2)
+        be='cpu'
+        be_args = {'rng_seed': self.seed}#, be : 'nervanagpu' }
+        (fstate_1shot, init_w_1shot ) = self.run_experiment_in_steps(config_file, N, 1, be, **be_args)
+
+        # run N steps in 2 shots
+        # first goes on the CPU
+        (fstate_half_shot, init_w_kshot ) = self.run_experiment_in_steps(config_file, N/2, 1, be, **be_args)
+        # use the prm file from the 1/2 run above to init this run and finish the full N epochs with GPU
+        be='gpu'
+        be_args = {'rng_seed': self.seed, be : 'nervanagpu' }
+        (fstate_kshot, temp_init_w ) = self.run_experiment_in_steps(config_file, N, 1, be, \
+                init_config=fstate_half_shot, **be_args)
+
+
+        # comapre the initial weights
+        assert self.check_init_weights( init_w_kshot, init_w_1shot , rtol=rtol, atol=atol ),\
+                'initial model weights were not the same'
+
+        #compare the final model state for 1 step of N epochs vs. k steps of N/k epochs
+        assert self.model_compare( fstate_1shot, fstate_kshot ,rtol=rtol, atol=atol),\
+                'final states of 1 vs %d step learning runs not matching' %k
+        return
+
+    #---------------------------------------------------
+    @attr('cuda','hey')  # can we do this test by test
+    def test_toyModel_compare_gpu_to_cpu(self):
+        # run whole test on cpu and run it 1/2 on cpu the 1/2 on gpu
+        # compare the outputs
+        N = 10 # for this tests make sure that N/k is not an integer
+        k = 2  # k should be 2 here
+        atol=0.0
+        rtol=.01
+
+        config_file = os.path.join( self.script_dir, \
+                'toy_serialize_check_base.yml' )
+
+        # run N steps in 2 shots and save the checkpoint which should be at int(N/2)
+        be='cpu'
+        be_args = {'rng_seed': self.seed}#, be : 'nervanagpu' }
+        (fstate_1shot, init_w_1shot ) = self.run_experiment_in_steps(config_file, N, 1, be, **be_args)
+
+        # run N steps in 2 shots
+        # first goes on the CPU
+        be='gpu'
+        be_args = {'rng_seed': self.seed, be : 'nervanagpu' }
+        (fstate_half_shot, init_w_kshot ) = self.run_experiment_in_steps(config_file, N/2, 1, be, **be_args)
+        # use the prm file from the 1/2 run above to init this run and finish the full N epochs with GPU
+        be='cpu'
+        be_args = {'rng_seed': self.seed}
+        (fstate_kshot, temp_init_w ) = self.run_experiment_in_steps(config_file, N, 1, be, \
+                init_config=fstate_half_shot, **be_args)
+
+
+        # comapre the initial weights
+        assert self.check_init_weights( init_w_kshot, init_w_1shot , rtol=rtol, atol=atol ),\
+                'initial model weights were not the same'
+
+        #compare the final model state for 1 step of N epochs vs. k steps of N/k epochs
+        assert self.model_compare( fstate_1shot, fstate_kshot ,rtol=rtol, atol=atol),\
+                'final states of 1 vs %d step learning runs not matching' %k
+        return
+
+
+
+    def run_experiment_in_steps(self, config_file, N, k, be, init_config=None, **be_args): 
+        # run an experiment for N epochs in k stepe with
+        # N/k epochs per step.  Last step will be enough epochs
+        # to reach N total epochs.  Between each step the 
+        # model will be serialized and saved, then reloaded
+        # from that saved file at the next step
+        # if init_config is not None, then that file will be used
+        #  as the initial deserialize file -  this is used for handing off 
+        #  models run on cpu.gpu backends to running on gpu/cpu backends
+
+        stepsize = N/k
+
+        for ind in range(1,k+1):
+            # run the same learning with N/k epochs k times
+            # each time saving and reloading the serialized model state
+            if ind == k:
+                end_epoch = N # in case N/k is a fraction, 
+                              # last step will end at epoch N
+            else:
+                end_epoch = ind*stepsize
+
+            # load up base experiment config
+            experiment = deserialize( config_file )
+
+            # run for N/k steps
+            experiment.model.num_epochs = end_epoch
+
+            if ind > 1:
+                # after step 1 need to load initial config from last runs serialized file
+                experiment.model.deserialized_path = last_saved_state
+            elif (init_config != None):
+                # used the given pickle file to initialize the mdoel
+                experiment.model.deserialized_path = init_config
+
+            # save the model to this file
+            last_saved_state = os.path.join(self.model_path,\
+                                        '%d_shot_%d.prm' %(k,end_epoch))
+            print last_saved_state
+            if os.path.exists(last_saved_state):
+                print 'removing %s' %last_saved_state
+                os.remove(last_saved_state)
+            experiment.model.serialized_path = last_saved_state
+
+            
+            experiment.model.serialize_schedule = k # may not be needed
+            backend = gen_backend(model=experiment.model, **be_args)
+            experiment.initialize(backend)
+
+            if ind == 1:
+                # save the initial weights for check with other runs
+                intial_weights={}
+                for ind,layer in enumerate(experiment.model.layers):
+                    if hasattr(layer,'weights'): # only looking at weights right now
+                        # make sure the names of each layer are unique by adding index
+                        intial_weights[ '%s_%d' %(layer.name, ind)  ] = \
+                                np.copy( layer.weights.asnumpyarray())
+
+            # run 
+            res_2shot_half = experiment.run()
+
+        return ( last_saved_state  , intial_weights )
 
 
     # load up two model pkl files and compare the data stored in them
@@ -223,7 +339,6 @@ class TestSerialization:
         if isinstance(obj1, np.ndarray):
             if not np.allclose(obj1, obj2, atol=atol, rtol=rtol):
                 maxdiff = np.amax( np.divide(np.absolute( obj1 - obj2 ) , np.absolute(obj2) ) )
-                import pdb;pdb.set_trace()
                 assert False, 'layer weight mismatch max %f' %maxdiff
         if isinstance(obj1, list):
             assert len(obj1) == len(obj2), 'list length mismatch'
