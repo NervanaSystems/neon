31c25974bfc11b6f90a338889f6d1d7a7939f360
Augustus Odena <odenaaugustus@gmail.com>
2015-08-06 13:04:04 -0700

    require channel_id to instantiate an ipc message region. Now can instantiate multiple regions simultaneously.

--- neon/ipc/client.py
+++ neon/ipc/client.py
@@ -5,11 +5,11 @@ logging.basicConfig(level=30)
 logger = logging.getLogger(__name__)
 
 print "client"
-client1 = Client()
-client1.start()
+client1 = Client(channel_id="one")
 
-client2 = Client()
-client2.start()
+client2 = Client(channel_id="two")
 
+# The below will fail
+#client3 = Client(channel_id="three")
 while True:
-    c = 8
+    pass
--- neon/ipc/server.py
+++ neon/ipc/server.py
@@ -6,10 +6,10 @@ logger = logging.getLogger(__name__)
 
 print "server"
 server1 = Server(req_size=1, res_size=1, channel_id="one")
-server1.start()
 
-#server2 = Server(req_size=1, res_size=1, channel_id="two")
-#server2.start()
+server2 = Server(req_size=1, res_size=1, channel_id="two")
 
+# The below will fail
+#server3 = Server(req_size=1, res_size=1) 
 while True:
-    c = 8
+    pass
