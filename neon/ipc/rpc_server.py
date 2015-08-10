234ed6653ee7aa69fd88adf954657f82be067160
Augustus Odena <odenaaugustus@gmail.com>
2015-08-10 10:41:51 -0700

    server is now a class as well.

--- neon/ipc/rpc_server.py
+++ neon/ipc/rpc_server.py
@@ -1,31 +1,36 @@
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-                            host='localhost'))
-
-channel = connection.channel()
-
-channel.queue_declare(queue='rpc_queue')
-
 def square(n):
     return n * n
 
-def on_request(ch,method,props,body):
-    n = int(body)
+class NeonRpcServer(object):
+    def __init__(self,rpc_queue):
 
-    print " computing square(%s)" % (n,)
-    response = square(n)
+        connection = pika.BlockingConnection(pika.ConnectionParameters(
+                                    host='localhost'))
 
-    ch.basic_publish(exchange='',
-                     routing_key=props.reply_to,
-                     properties=pika.BasicProperties(
-                        correlation_id=props.correlation_id),
-                     body=str(response))
+        channel = connection.channel()
 
-    ch.basic_ack(delivery_tag = method.delivery_tag)
+        channel.queue_declare(queue='rpc_queue')
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(on_request, queue='rpc_queue')
+        channel.basic_qos(prefetch_count=1)
+        channel.basic_consume(self.on_request, queue='rpc_queue')
 
-print " Serving requests"
-channel.start_consuming()
+        print " Serving requests"
+        channel.start_consuming()
+
+    def on_request(self,ch,method,props,body):
+        n = int(body)
+
+        print " computing square(%s)" % (n,)
+        response = square(n)
+
+        ch.basic_publish(exchange='',
+                         routing_key=props.reply_to,
+                         properties=pika.BasicProperties(
+                            correlation_id=props.correlation_id),
+                         body=str(response))
+
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+
+server = NeonRpcServer('rpc_queue')
