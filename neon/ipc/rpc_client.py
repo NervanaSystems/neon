8fd46f66edabbdb4f17c1db25bc8e0323e39f1fc
Augustus Odena <odenaaugustus@gmail.com>
2015-08-10 10:03:07 -0700

    working example rpc server and client.

---
+++ neon/ipc/rpc_client.py
@@ -0,0 +1,39 @@
+import pika
+import uuid
+
+class FibonacciRpcClient(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+                host='localhost'))
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(self.on_response, no_ack=True,
+                                   queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='',
+                                   routing_key='rpc_queue',
+                                   properties=pika.BasicProperties(
+                                         reply_to = self.callback_queue,
+                                         correlation_id = self.corr_id,
+                                         ),
+                                   body=str(n))
+        while self.response is None:
+            self.connection.process_data_events()
+        return int(self.response)
+
+fibonacci_rpc = FibonacciRpcClient()
+
+print " [x] Requesting fib(30)"
+response = fibonacci_rpc.call(30)
+print " [.] Got %r" % (response,)
---
+++ neon/ipc/rpc_server.py
@@ -0,0 +1,31 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+                            host='localhost'))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='rpc_queue')
+
+def square(n):
+    return n * n
+
+def on_request(ch,method,props,body):
+    n = int(body)
+
+    print " computing square(%s)" % (n,)
+    response = square(n)
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(
+                        correlation_id=props.correlation_id),
+                     body=str(response))
+
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(on_request, queue='rpc_queue')
+
+print " Serving requests"
+channel.start_consuming()
