Add safelog - Add safelog backend function for cost display - Remove epsilon argument from cost functions - Correct auxiliary relu function in nervanacpu - Correct weight initialization scaling for pre-dropout layers for caffe-style dropout - Style fixes - Remove some examples

neon/backends/nervanacpu.py
@@ -445,6 +445,7 @@ numpy_call_dict = {
     "sqr": lambda left: np.square(left),
     "exp": lambda left: np.exp(left),
     "log": lambda left: np.log(left),
+    "safelog": lambda left: np.log(np.maximum(left, np.exp(-50.))),
     "exp2": lambda left: np.exp2(left),
     "log2": lambda left: np.log2(left),
     "sig": lambda left: 1. / (1. + np.exp(-left)),
@@ -1380,6 +1381,6 @@ class NervanaCPU(Backend):
             out: reference to output
         """
         if out is not None:
-            np.log(1 + np.exp(ary), out)
+            return np.maximum(ary, 0, out)
         else:
-            return np.log(1 + np.exp(ary))
+            return np.maximum(ary, 0)

neon/transforms/cost.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 # ----------------------------------------------------------------------------
 from neon import NervanaObject
+import numpy as np
 
 
 class Cost(NervanaObject):
@@ -85,15 +86,13 @@ class CrossEntropyBinary(Cost):
         bprop assumes that shortcut is used to calculate derivative
     """
 
-    def __init__(self, epsilon=2 ** -23, scale=1):
+    def __init__(self, scale=1):
         """
         Initialize the binary cross entropy function
 
         Args:
-            epsilon (float): set the epsilon
-                             (small number to prevent log(0) errors)
+            scale (float): amount by which to scale the backpropagated error
         """
-        self.epsilon = epsilon
         self.scale = scale
 
     def __call__(self, y, t):
@@ -107,8 +106,8 @@ class CrossEntropyBinary(Cost):
         Returns:
             OpTree: Returns the binary cross entropy cost
         """
-        a = - self.be.log(y + self.epsilon) * t
-        b = - self.be.log(1 - y + self.epsilon) * (1 - t)
+        a = - self.be.safelog(y) * t
+        b = - self.be.safelog(1 - y) * (1 - t)
         return self.be.sum(a + b, axis=0)
 
     def bprop(self, y, t):
@@ -135,18 +134,16 @@ class CrossEntropyMulti(Cost):
         bprop assumes that shortcut is used to calculate derivative
     """
 
-    def __init__(self, epsilon=2 ** -23, scale=1, usebits=False):
+    def __init__(self, scale=1, usebits=False):
         """
         Initialize the multiclass cross entropy function
 
         Args:
-            epsilon (float): set the epsilon
-                             (small number to prevent log(0) errors)
+            scale (float): amount by which to scale the backpropagated error
             usebits (boolean): whether to display costs in bits or nats (default)
         """
-        self.epsilon = epsilon
         self.scale = scale
-        self.logfunc = self.be.log2 if usebits else self.be.log
+        self.logscale = np.float(1. / np.log(2.0) if usebits else 1.)
 
     def __call__(self, y, t):
         """
@@ -159,7 +156,7 @@ class CrossEntropyMulti(Cost):
         Returns:
             OpTree: Returns the multiclass cross entropy cost
         """
-        return (self.be.sum(-t * self.logfunc(self.be.clip(y, self.epsilon, 1.0)), axis=0))
+        return (self.be.sum(-t * self.logscale * self.be.safelog(y), axis=0))
 
     def bprop(self, y, t):
         """
@@ -238,7 +235,7 @@ class TopKMisclassification(Metric):
         nEq = be.sum(y == self.correctProbs, axis=0)
         self.topk[:] = 1. - (nSlots > 0) * ((nEq <= nSlots) * (1 - nSlots / nEq) + nSlots / nEq)
         self.top1[:] = 1. - (be.max(y, axis=0) == self.correctProbs) / nEq
-        self.correctProbs[:] = -be.log(self.correctProbs)
+        self.correctProbs[:] = -be.safelog(self.correctProbs)
 
         return self.outputs.get().mean(axis=1)
 

tests/test_costs.py
@@ -43,9 +43,10 @@ def compare_tensors(func, y, t, outputs, deriv=False, tol=0.):
 def test_cross_entropy_binary(backend_default):
     outputs = np.array([0.5, 0.9, 0.1, 0.0001]).reshape((4, 1))
     targets = np.array([0.5, 0.99, 0.01, 0.2]).reshape((4, 1))
-    eps = 2 ** -23
-    expected_result = np.sum((-targets * np.log(outputs + eps)) -
-                             (1 - targets) * np.log(1 - outputs + eps),
+    eps = np.exp(-50)
+    expected_log = np.log(np.maximum(outputs, eps))
+    expected_mlog = np.log(np.maximum(1 - outputs, eps))
+    expected_result = np.sum((-targets * expected_log) - (1 - targets) * expected_mlog,
                              keepdims=True)
     compare_tensors(CrossEntropyBinary(),
                     outputs, targets, expected_result, tol=1e-6)
@@ -54,9 +55,10 @@ def test_cross_entropy_binary(backend_default):
 def test_cross_entropy_binary_limits(backend_default):
     outputs = np.array([0.5, 1.0, 0.0, 0.0001]).reshape((4, 1))
     targets = np.array(([0.5, 0.0, 1.0, 0.2])).reshape((4, 1))
-    eps = 2 ** -23
-    expected_result = np.sum((-targets * np.log(outputs + eps)) -
-                             (1 - targets) * np.log(1 - outputs + eps),
+    eps = np.exp(-50)
+    expected_log = np.log(np.maximum(outputs, eps))
+    expected_mlog = np.log(np.maximum(1 - outputs, eps))
+    expected_result = np.sum((-targets * expected_log) - (1 - targets) * expected_mlog,
                              keepdims=True)
     compare_tensors(CrossEntropyBinary(),
                     outputs, targets, expected_result, tol=1e-5)
@@ -79,9 +81,9 @@ def test_cross_entropy_binary_derivative(backend_default):
 def test_cross_entropy_multi(backend_default):
     outputs = np.array([0.5, 0.9, 0.1, 0.0001]).reshape((4, 1))
     targets = np.array([0.5, 0.99, 0.01, 0.2]).reshape((4, 1))
-    eps = 2 ** -23
-    expected_result = np.sum(-targets * np.log(np.clip(outputs, eps, 1.0)),
-                             axis=0, keepdims=True)
+    eps = np.exp(-50)
+    expected_log = np.log(np.maximum(outputs, eps))
+    expected_result = np.sum(-targets * expected_log, axis=0, keepdims=True)
     compare_tensors(CrossEntropyMulti(),
                     outputs, targets, expected_result, tol=1e-6)
 
@@ -89,9 +91,9 @@ def test_cross_entropy_multi(backend_default):
 def test_cross_entropy_multi_limits(backend_default):
     outputs = np.array([0.5, 1.0, 0.0, 0.0001]).reshape((4, 1))
     targets = np.array(([0.5, 0.0, 1.0, 0.2])).reshape((4, 1))
-    eps = 2 ** -23
-    expected_result = np.sum(-targets * np.log(np.clip(outputs, eps, 1.0)),
-                             axis=0, keepdims=True)
+    eps = np.exp(-50)
+    expected_log = np.log(np.maximum(outputs, eps))
+    expected_result = np.sum(-targets * expected_log, axis=0, keepdims=True)
     compare_tensors(CrossEntropyMulti(),
                     outputs, targets, expected_result, tol=1e-5)
 