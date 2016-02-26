Updates for datatype changes -update examples for imageloader arguments -remove old serialization check -update diagnostics for allclose -fix batch norm gradient test

neon/backends/__init__.py
@@ -43,8 +43,8 @@ def gen_backend(backend='cpu', rng_seed=None, datatype=np.float32,
                                       random number generator of the instantiated backend.
                                       Defaults to None, which doesn't explicitly seed (so each run
                                       will be different)
-        dataype (dtype): Default tensor data type. CPU backend supports np.float64, np.float32 and
-                         np.float16; GPU backend supports np.float32 and np.float16.
+        datatype (dtype): Default tensor data type. CPU backend supports np.float64, np.float32,
+                          and np.float16; GPU backend supports np.float32 and np.float16.
         batch_size (int): Set the size the data batches.
         stochastic_round (int/bool, optional): Set this to True or an integer to implent
                                                stochastic rounding. If this is False rounding will

neon/layers/layer.py
@@ -1448,6 +1448,7 @@ class BatchNorm(Layer):
         self.gamma = None
         self.gmean = None
         self.gvar = None
+        self.stats_dtype = np.float64 if self.be.default_dtype is np.float64 else np.float32
 
     def __str__(self):
         return "BatchNorm Layer '%s': %d inputs, %d steps, %d feature maps" % (
@@ -1463,19 +1464,19 @@ class BatchNorm(Layer):
     def allocate(self, shared_outputs=None):
         super(BatchNorm, self).allocate(shared_outputs)
         self.y = self.outputs.reshape((self.nfm, -1))
-        self.xvar = self.be.zeros((self.nfm, 1), dtype=np.float32)
+        self.xvar = self.be.zeros((self.nfm, 1), dtype=self.stats_dtype)
         if self.allparams is None:
             self.init_params(self.nfm)
         if self.prev_layer is None or self.prev_layer.batch_sum is None:
-            self.xsum = self.be.zeros((self.nfm, 1), dtype=np.float32)
+            self.xsum = self.be.zeros((self.nfm, 1), dtype=self.stats_dtype)
             self.compute_batch_sum = True
         else:
             self.xsum = self.prev_layer.batch_sum
             self.compute_batch_sum = False
 
     def init_params(self, dim0):
-        self.beta = self.be.zeros((dim0, 1), dtype=np.float32, **self.get_param_attrs())
-        self.gamma = self.be.ones((dim0, 1), dtype=np.float32, **self.get_param_attrs())
+        self.beta = self.be.zeros((dim0, 1), dtype=self.stats_dtype, **self.get_param_attrs())
+        self.gamma = self.be.ones((dim0, 1), dtype=self.stats_dtype, **self.get_param_attrs())
         self.params = [self.beta, self.gamma]
 
         self.grad_params = [self.be.zeros_like(p) for p in self.params]

tests/utils.py
@@ -51,7 +51,7 @@ def allclose_with_out(x, y, atol=0.0, rtol=1.0e-5):
               % (np.median(dd), np.min(dd), np.max(dd), atol)
         amax = np.argmax(dd)
         print 'worst case: %e %e' % (x.flat[amax], y.flat[amax])
-        dd = np.abs(np.divide(dd, y))
+        dd = np.minimum(np.abs(dd - atol), 0) / np.abs(y)
         print 'rel errors: %e [%e, %e] Rel Thresh = %e' \
               % (np.median(dd), np.min(dd), np.max(dd), rtol)
         amax = np.argmax(dd)