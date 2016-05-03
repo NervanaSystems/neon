4f292748917ac07b356f05d1067b55b84e978c14
Anil Thomas <anil@nervanasys.com>
2016-05-03 09:49:09 -0700

    Perform FFT in batch mode

--- loader/src/specgram.hpp
+++ loader/src/specgram.hpp
@@ -16,6 +16,7 @@
 #include "media.hpp"
 
 #include <sstream>
+#include <math.h>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -37,61 +38,52 @@ public:
         }
 
         _maxSignalSize = params->_maxDuration * params->_samplingFreq;
+        _buf = new char[4 *  _maxSignalSize];
+        _image = new Mat(_timeSteps, _numFreqs, CV_8UC1);
+        _window = new Mat(1, _windowSize, CV_32FC1);
+        // TODO: pick the right window function
+        hann(_windowSize - 1);
+    }
+
+    virtual ~Specgram() {
+        delete _window;
+        delete _image;
+        delete[] _buf;
     }
 
     void generate(RawMedia* raw, char* buf, int bufSize) {
         // TODO: get rid of this assumption
         assert(raw->sampleSize() == 2);
         assert(_timeSteps * _numFreqs == bufSize);
-        int signalSize = raw->dataSize() / raw->sampleSize();
-        if (signalSize > _maxSignalSize) {
-            stringstream ss;
-            ss << "Found example with duration larger than "
-               << _maxDuration << " seconds\n";
-            throw std::runtime_error(ss.str());
-        }
-        assert(signalSize >= _windowSize);
-        Mat signal(1, signalSize, CV_16UC1, (ushort*) raw->getBuf(0));
+        int rows = stridedSignal(raw);
+        assert(rows <= _timeSteps);
+        Mat signal(rows, _windowSize, CV_16UC1, (ushort*) _buf);
         Mat input;
         signal.convertTo(input, CV_32FC1);
-        assert(input.cols == signalSize);
 
-        Mat image(_timeSteps, _numFreqs, CV_8UC1);
-        int count = ((signalSize - _windowSize) / _stride) + 1;
-        assert(count <= _timeSteps);
-        // TODO: do FFT in batch mode instead of looping.
-        for (int i = 0; i < count; i++) {
-            int startCol = i * _stride;
-            int endCol = startCol + _windowSize;
-            assert(endCol <= input.cols);
-            Mat slice = input(Range::all(), Range(startCol, endCol));
-            // TODO: apply window
-            Mat planes[] = {slice, Mat::zeros(slice.size(), CV_32FC1)};
-            Mat compx;
-            cv::merge(planes, 2, compx);
+        applyWindow(input);
+        Mat planes[] = {input, Mat::zeros(input.size(), CV_32FC1)};
+        Mat compx;
+        cv::merge(planes, 2, compx);
 
-            cv::dft(compx, compx);
-            compx = compx(Range::all(), Range(0, _numFreqs));
+        cv::dft(compx, compx, cv::DFT_ROWS);
+        compx = compx(Range::all(), Range(0, _numFreqs));
 
-            cv::split(compx, planes);
-            cv::magnitude(planes[0], planes[1], planes[0]);
-            Mat mag = planes[0];
+        cv::split(compx, planes);
+        cv::magnitude(planes[0], planes[1], planes[0]);
+        Mat mag = planes[0];
 
-            cv::log(mag, mag);
-            cv::normalize(mag, mag, 0, 1, CV_MINMAX);
-            mag *= 255;
-
-            Mat bytes;
-            mag.convertTo(bytes, CV_8UC1);
-            bytes.row(0).copyTo(image.row(i));
+        cv::log(mag, mag);
+        for (int i = 0; i < rows; i++) {
+            cv::normalize(mag.row(i), _image->row(i), 0, 255, CV_MINMAX, CV_8UC1);
         }
+
         // Pad the rest with zeros.
-        for (int i = count; i < _timeSteps; i++) {
-            image.row(i) = cv::Scalar::all(0);
-        }
+        (*_image)(Range(rows, _image->rows), Range::all()) = cv::Scalar::all(0);
 
+        // Rotate by 90 degrees.
         Mat result(_numFreqs, _timeSteps, CV_8UC1, buf);
-        cv::transpose(image, result);
+        cv::transpose(*_image, result);
         cv::flip(result, result, 0);
     }
 
@@ -103,6 +95,39 @@ private:
         return (num == 1);
     }
 
+    void hann(int steps) {
+        for (int i = 0; i <= steps; i++) {
+            _window->at<float>(0, i) = 0.5 - 0.5 * cos((2.0 * PI * i) / steps);
+        }
+    }
+
+    void applyWindow(Mat& signal) {
+        for (int i = 0; i < signal.rows; i++) {
+            signal.row(i) = signal.row(i).mul((*_window));
+        }
+    }
+
+    int stridedSignal(RawMedia* raw) {
+        int signalSize = raw->dataSize() / raw->sampleSize();
+        if (signalSize > _maxSignalSize) {
+            signalSize = _maxSignalSize;
+        }
+        assert(signalSize >= _windowSize);
+        int count = ((signalSize - _windowSize) / _stride) + 1;
+        assert(count <= _timeSteps);
+        char* src = raw->getBuf(0);
+        char* dst = _buf;
+        int windowSizeInBytes = _windowSize * raw->sampleSize();
+        int strideInBytes = _stride * raw->sampleSize();
+        for (int i = 0; i < count; i++) {
+            memcpy(dst , src, windowSizeInBytes);
+            dst += windowSizeInBytes;
+            src += strideInBytes;
+        }
+
+        return count;
+    }
+
 private:
     int                         _maxDuration;
     // Window size and stride are in terms of samples.
@@ -111,4 +136,8 @@ private:
     int                         _timeSteps;
     int                         _numFreqs;
     int                         _maxSignalSize;
+    char*                       _buf;
+    Mat*                        _image;
+    Mat*                        _window;
+    constexpr static double     PI = 3.14159265358979323846;
 };
