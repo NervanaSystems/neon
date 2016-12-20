5dbf83c8b04dec9487b3970fceee7fa5558a8369
Zach Dwiel <zach@nervanasys.com>
2016-12-20 15:06:37 -0800

    add MultiMetric to allow wrapping Metric classes with an index so they can be used in models with multiple outputs (models which use the Tree container)

--- neon/transforms/cost.py
+++ neon/transforms/cost.py
@@ -283,6 +283,40 @@ class Metric(Cost):
         raise NotImplementedError()
 
 
+class MultiMetric(Metric):
+    """
+    A wrapper Metric which can be used with Tree models which have more than
+    one output.  Tree models have tuples of tensors, one tensor per output.
+    Wrapping a Metric with a MultiMetric ensures that the metric sees only one
+    of those tensors in the output tuple instead of all of them.
+    """
+
+    def __init__(self, metric, index):
+        """
+        Args:
+            metric (Metric): Metric to apply in this multi-output context
+            index (integer): The index into the model's output tuple to apply
+                             the metric to
+        """
+        self.metric = metric
+        self.index = index
+
+    def __call__(self, y, t, *args, **kwargs):
+        """
+        Args:
+            y (Tensor or OpTree): Output of previous layer or model
+            t (Tensor or OpTree): True targets corresponding to y
+
+        Returns:
+            numpy array : Returns the log loss  metric in numpy array,
+                         [LogLoss]
+        """
+        return self.metric(y[self.index], y[self.index], *args, **kwargs)
+
+    def __getattr__(self, key):
+        return getattr(self.metric, key)
+
+
 class LogLoss(Metric):
 
     """
