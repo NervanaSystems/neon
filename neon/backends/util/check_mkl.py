ed263c65eddc06c9d82a45b8889598de10293e96
Peng Zhang <peng.a.zhang@intel.com>
2017-12-19 13:37:29 +0800

    Fix misleading message (#1118)
    * fix skip node and test_mergesum_layer for issue916
    
    * change error message when mkl is not correctly installed

--- neon/backends/util/check_mkl.py
+++ neon/backends/util/check_mkl.py
@@ -24,23 +24,23 @@ def get_mkl_lib(device_id=None, verbose=False):
         current_path = os.path.dirname(os.path.realpath(__file__))
         mkl_engine_path = os.path.join(current_path, os.pardir, 'mklEngine', 'mklml.dll')
         if not os.path.isfile(mkl_engine_path):
-            neon_logger.display("mklml.dll not found; falling back to cpu backend")
+            neon_logger.display("mklml.dll not found")
             return 0
 
         mkl_engine_path = os.path.join(current_path, os.pardir, 'mklEngine', 'mklEngine.dll')
         if not os.path.isfile(mkl_engine_path):
-            neon_logger.display("mklEngine.dll not found; falling back to cpu backend")
+            neon_logger.display("mklEngine.dll not found")
             return 0
 
         math_engine_path = os.path.join(current_path, os.pardir, 'mklEngine', 'cmath.dll')
         if not os.path.isfile(math_engine_path):
-            neon_logger.display("cmath.dll not found; falling back to cpu backend")
+            neon_logger.display("cmath.dll not found")
             return 0
 
         header_path = os.path.join(os.path.dirname(__file__), 'mklEngine',
                                    'src', 'math_cpu.header')
         if os.path.isfile(header_path):
-            neon_logger.display("math_cpu.header not found; falling back to cpu backend")
+            neon_logger.display("math_cpu.header not found")
             return 0
         return 1
 
@@ -49,17 +49,17 @@ def get_mkl_lib(device_id=None, verbose=False):
         current_path = os.path.dirname(os.path.realpath(__file__))
         mkl_engine_path = os.path.join(current_path, os.pardir, 'mklEngine', 'mklEngine.so')
         if not os.path.isfile(mkl_engine_path):
-            neon_logger.display("mklEngine.so not found; falling back to cpu backend")
+            neon_logger.display("mklEngine.so not found")
             return 0
 
         math_engine_path = os.path.join(current_path, os.pardir, 'mklEngine', 'cmath.so')
         if not os.path.isfile(math_engine_path):
-            neon_logger.display("cmath.so not found; falling back to cpu backend")
+            neon_logger.display("cmath.so not found")
             return 0
 
         header_path = os.path.join(os.path.dirname(__file__), 'mklEngine',
                                    'src', 'math_cpu.header')
         if os.path.isfile(header_path):
-            neon_logger.display("math_cpu.header not found; falling back to cpu backend")
+            neon_logger.display("math_cpu.header not found")
             return 0
         return 1
